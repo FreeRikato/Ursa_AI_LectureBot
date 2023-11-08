b55931f51d183f0d4f7dd4d828fcfc1267ca3f40
FreeRikato <aravinthan@student.tce.edu>
2023-11-08 13:24:41 +0530

    Add SRT to CSV converter script

--- SRT_to_CSV_converter.py
+++ SRT_to_CSV_converter.py
@@ -1,12 +1,26 @@
 import csv
 import re
+# SRT files are used to store subtitles for videos. They look like this:
+# 1
+#00:00:01,000 --> 00:00:04,000
+#This is the first subtitle.
+#
+#2
+#00:00:05,000 --> 00:00:08,000
+#This is the second subtitle.
+#
+#3
+#00:00:09,000 --> 00:00:12,000
+#This is the third subtitle.
 
 # Function to parse an SRT file
 def parse_srt(srt_file_path):
     with open(srt_file_path, 'r', encoding='utf-8-sig') as file:
+        # The 'utf-8-sig' encoding is a variant of UTF-8 that includes a Byte Order Mark (BOM). It's used to signal that a file is encoded in UTF-8. This can be useful when the file is being read by a system that doesn't default to UTF-8.
         content = file.read()
         # Split content at blank lines
         subtitles = re.split(r'\n\s*\n', content.strip(), flags=re.UNICODE)
+        # The re.UNICODE flag makes the pattern behave as a Unicode character set. This means that the special sequences \w, \W, \b, \B, \d, \D, \s and \S will match based on the Unicode character properties database.
         parsed_subtitles = []
         for subtitle in subtitles:
             parts = subtitle.split('\n')
